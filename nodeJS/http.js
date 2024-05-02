6f283a4978bc094ad9a029465e1369f4a8d7eed9
jeevan-s23 <jeevans2224@gmail.com>
2024-05-02 10:35:50 +0530

    node js files

--- nodeJS/http.js
+++ nodeJS/http.js
@@ -2,12 +2,13 @@
 
 let nameElement = document.querySelector('.name');
 let surnameElement = document.querySelector('.surname')
+let surnameInput = document.querySelector('#surnameInput');
 
-const getMyname = async () =>{
-    let res = await fetch('https://127.0.0.1:3000/')
+const getMyName = async () => {
+    let res = await fetch('http://127.0.0.1:3000/')
     console.log(res);
-    if (!res.ok) {
-        alert('failed to get name');
+    if(!res.ok) {
+        alert('Failed to get name');
         return;
     }
     let name = await res.text();
@@ -15,17 +16,20 @@ const getMyname = async () =>{
 }
 
 
-const postsurname = async () => {
-    let res = await fetch('https://127.0.0.1:3000/',{
-        method: 'post',
+const postSurname = async () => {
+    let mysurname = surnameInput.value;
+    let res = await fetch('http://127.0.0.1:3000/', {
+        method: 'POST',
         headers: {
-            'content-type':'application/json'
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({surname:'kumar'})
+        body: JSON.stringify({surname: Sampath})
     })
-    console.log(res);
+
+
+    // console.log(res);
     if(!res.ok) {
-        alert('failed to post surname');
+        alert('Failed to post surname');
         return;
     }
     let { surname } = await res.json();
--- nodeJS/server.mjs
+++ nodeJS/server.mjs
@@ -3,36 +3,59 @@ import { createServer } from 'node:http';
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((req, res) => {
-    console.log('Received request....', req.url, req.method);
+const server = createServer( async (req, res) => {
+  console.log('Received request...', req.url, req.method);
+  
 
-    if (req.url == '/') {
-        if (req.method == 'GET') {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'Text/plain');
-            res.setHeader('Access-control-Allow-Orign', "*");
+  if(req.url == '/') {
+    if(req.method == 'GET') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Access-Control-Allow-Origin', "*");
 
-            res.end("Harshith");
-        }
-        if (req.method == 'OPTION') {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-            res.setHeader('Access-Control-Allow-Methods', "POSt, GET, OPTION");
-            res.setHeader('Access-Control-Allow-Headers', '*');
-            res.end('OK');
-        }
-        if (req.method == 'POST') {
-            console.log('post method...')
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.setHeader('Access-Control-Allow-Origin', "*");
 
-            res.end(JSON.stringify({surname: "Srinivas"}));
-        }
+      res.end("JEEVAN S");
+      // console.log(res)
     }
-}); 
+
+
+    if(req.method == 'OPTIONS') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'appliction/json');
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+      res.setHeader('Access-Control-Allow-Methods', "POST, GET, OPTIONS");
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      res.end('OK');
+    }
+
+    if(req.method == 'POST') {
+      let data = '';
+      req.on('data', chunk => {
+        data += chunk;
+      });
+      req.on('end', () => {
+        try {
+ 
+
+          const userData = JSON.parse(data);
+          const {surname} = userData;
+          console.log('Surname...', surname);
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Access-Control-Allow-Origin', "*");
+          res.end(JSON.stringify({surname}));
+        } catch (e) {
+          
+          console.error('Error parsing JSON: ', e.message);
+          res.writeHead(400, { 'Content-Type': 'text/plain'});
+          res.end('Invalid JSON data');
+        }
+      })
+    }
+  }
+  
+});
 
 server.listen(port, hostname, () => {
-    console.log('Server running at http://$(hostname):$(port)/');
-})
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
